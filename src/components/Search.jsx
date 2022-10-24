b8ee43fd09a5fd19c420428f5883696117a39834
Brad Green <green.brad@live.com>
2022-10-24 17:00:24 -0400

    new search bar, chore: still needs indexing

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,9 @@
-import { useCallback, useEffect, useState } from 'react'
-import { createPortal } from 'react-dom'
+//import { useCallback, useEffect, useState } from 'react'
+//import { createPortal } from 'react-dom'
 import Link from 'next/link'
-import Router from 'next/router'
-import { DocSearchModal, useDocSearchKeyboardEvents } from '@docsearch/react'
+//import Router from 'next/router'
+//import { DocSearchModal, useDocSearchKeyboardEvents } from '@docsearch/react'
+import { useRef } from 'react'
 
 const docSearchConfig = {
   appId: process.env.NEXT_PUBLIC_DOCSEARCH_APP_ID,
@@ -22,6 +23,36 @@ function SearchIcon(props) {
   )
 }
 
+export function Search() {
+  const clickPoint = useRef();
+  const handleFocus = () => {
+      clickPoint.current.style.display = "none";
+  };
+
+  const handleBlur = () => {
+      clickPoint.current.style.display = "block";
+  };
+
+  return (
+      <div className="items-center px-4 flex justify-center" >
+          <div className="relative mr-3">
+              <div className="absolute top-3 left-3 items-center" ref={clickPoint}>
+                  <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+              </div>
+              <input
+                  type="text"
+                  className="block p-2 pl-10 w-70 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:pl-3"
+                  placeholder="Search Here..."
+                  onFocus={handleFocus}
+                  onBlur={handleBlur}
+              />
+          </div>
+      </div>
+  );
+}
+
+/* Old Search Component */
+/*
 export function Search() {
   let [isOpen, setIsOpen] = useState(false)
   let [modifierKey, setModifierKey] = useState()
@@ -78,3 +109,5 @@ export function Search() {
     </>
   )
 }
+*/
+
